543810f375f3e9678fe4ed1688b1062db6088d4c
Juntao Qiu <juntao.qiu@gmail.com>
2018-09-12 14:50:09 +1000

    footer

--- export.js
+++ export.js
@@ -22,13 +22,23 @@ class Resume {
     const browser = await Puppeteer.launch()
     const page = await browser.newPage()
 
+    const cssb = [];
+    cssb.push('<style>');
+    cssb.push('h1 { font-family: "Open Sans"; font-weight: 100; font-size:10px; position:absolute; background-color:#f00; z-index:1000; color:#333; text-align:center; margin-left:64px;}');
+    cssb.push('</style>');
+
+    const css = cssb.join('');
+
     await page.setContent(html)
     await page.pdf({
       path: 'index.pdf', 
       format: 'A4', 
+      displayHeaderFooter: true,
+      headerTemplate: css+'',
+      footerTemplate: css+'<h1>© 2018 ThoughtWorks, Ltd. Confidential – do not distribute</h1>',
       margin: { 
-        top: "20px", 
-        bottom: "20px",
+        top: "40px", 
+        bottom: "40px",
         right: "30px",
         left: "30px",
       }
